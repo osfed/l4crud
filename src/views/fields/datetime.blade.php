e422e64173868303796096b2b857f255e45d5a9a
osfed <osfed2709@gmail.com>
2014-10-13 11:22:49 -0700

    Actualización de vista

--- src/views/fields/datetime.blade.php
+++ src/views/fields/datetime.blade.php
@@ -1,20 +1,33 @@
 <?php 
-$value = date('Y-m-d H:i:s');
+$value = date('Y-m-d H:i');
 
 if (isset($raw->item->$key))
     $value = $raw->item->$key;
 elseif (isset($field['default']))
 {
-	if ($field['default'] == 'date')
-    	$value = date('Y-m-d');
+    if ($field['default'] == 'date')
+        $value = date('Y-m-d');
     else
-    	$value = $field['default'];
+        $value = $field['default'];
 }
-?><div class="form-group">
+?>
+<div class="form-group">
     <label for="field_<?php echo $key ?>"><?php echo $field['title']?></label>
-    <?php if ((isset($field['readonly']) && ($raw->getState() == 'add' || $raw->getState() == 'edit')) || $raw->getState() == 'view'):?>
-	<p class="form-control-static"><?php echo $value ?></p>
-	<?php else:?>
-    <input name="<?php echo $key ?>" id="field_<?php echo $key ?>" type="text" class="form-control" placeholder="<?php echo $value ?>" value="<?php echo $value ?>"/>
+    <div class='input-group date'>        
+        <?php if ((isset($field['readonly']) && ($raw->getState() == 'add' || $raw->getState() == 'edit')) || $raw->getState() == 'view'):?>
+        <p class="form-control-static"><?php echo $value ?></p>
+        <?php else:?>
+        <input name="<?php echo $key ?>" id="field_<?php echo $key ?>" type="text" class="form-control" placeholder="<?php echo $value ?>" value="<?php echo $value ?>"/>
+        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+    </div>
 <?php endif;?>
 </div>
+
+            
+<script type="text/javascript">
+    $(function () {
+        $('.date').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm'            
+        });
+    });
+</script>    
